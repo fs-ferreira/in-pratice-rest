ff995d0912ed22a30e1db4df6590e7340ec03f08
Felipe Ferreira <felipeferreira0811@gmail.com>
2024-02-15 13:15:51 -0300

    15/02/24 - Refinamento do projeto e remoção de warnings

--- src/main/java/br/com/fsferreira/exceptions/ResourceNotFoundException.java
+++ src/main/java/br/com/fsferreira/exceptions/ResourceNotFoundException.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public ResourceNotFoundException(String ex) {
-		super(ex);
-	}
+    public ResourceNotFoundException(String ex) {
+        super(ex);
+    }
 }
--- src/main/java/br/com/fsferreira/services/PersonService.java
+++ src/main/java/br/com/fsferreira/services/PersonService.java
@@ -1,9 +1,6 @@
 package br.com.fsferreira.services;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
